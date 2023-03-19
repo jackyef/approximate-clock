0c2330a31619854ffe38c5ec5e3b8e77c1f7b80d
jackyef <jacky.efendi1@gmail.com>
2023-03-20 00:34:34 +0700

    Implement actual clock

--- src/components/Clock/ActualClock.tsx
+++ src/components/Clock/ActualClock.tsx
@@ -1,73 +1,78 @@
+import { forwardRef } from "react";
 import clsx from "clsx";
 import { useAtomValue } from "jotai";
+import { AnimatePresence, motion } from "framer-motion";
 import { hourAtom, minuteAtom, secondAtom } from "./atoms";
 
-const SingleCharacter = (props: { character: string }) => {
-  return <div className={clsx("inline-block tabular-nums")}>{props.character}</div>;
-};
-
-const Hours = () => {
-  const hour = useAtomValue(hourAtom);
-  const digits = String(hour % 12).split("");
-
-  if (digits.length === 1) digits.unshift("0");
-
+const SingleCharacter = forwardRef((props: { character: string }, ref) => {
   return (
-    <>
-      {digits.map((character, index) => (
-        <SingleCharacter key={`h-${index}`} character={character} />
-      ))}
-    </>
+    <motion.div
+      // @ts-ignore
+      ref={ref}
+      className={clsx("inline-block tabular-nums")}
+      initial={{ y: -15, opacity: 0, rotateX: '90deg' }}
+      animate={{ y: 0, opacity: 1, rotateX: '0deg' }}
+      exit={{ y: 15, opacity: 0, rotateX: '90deg' }}
+      transition={{ duration: 0.3 }}
+    >
+      {props.character}
+    </motion.div>
   );
-};
+});
 
-const Minutes = () => {
-  const minute = useAtomValue(minuteAtom);
-  const digits = String(minute).split("");
-
-  if (digits.length === 1) digits.unshift("0");
-
-  return (
-    <>
-      {digits.map((character, index) => (
-        <SingleCharacter key={`m-${index}`} character={character} />
-      ))}
-    </>
-  );
-};
-const Seconds = () => {
-  const second = useAtomValue(secondAtom);
-  const digits = String(second).split("");
-
-  if (digits.length === 1) digits.unshift("0");
-
-  return (
-    <>
-      {digits
-        .map((character, index) => (
-          <SingleCharacter key={`s-${index}`} character={character} />
-        ))}
-    </>
-  );
-};
+SingleCharacter.displayName = "SingleCharacter";
 
 export const ActualClock = () => {
   const hour = useAtomValue(hourAtom);
+  const hourDigits = String(hour % 12).split("");
+
+  if (hourDigits.length === 1) hourDigits.unshift("0");
+
   const amOrPm = hour < 12 ? "AM" : "PM";
 
+  const second = useAtomValue(secondAtom);
+  const secondDigits = String(second).split("");
+
+  if (secondDigits.length === 1) secondDigits.unshift("0");
+
+  const minute = useAtomValue(minuteAtom);
+  const minuteDigits = String(minute).split("");
+
+  if (minuteDigits.length === 1) minuteDigits.unshift("0");
+
   return (
-    <div className={clsx('text-xl')}>
-      <Hours />
-      <div className="inline-block w-1" />
-      <Minutes />
-      <div className="inline-block w-1" />
-      <Seconds />
-      <div className="inline-block w-1" />
-      {String(amOrPm)
-        .split("")
-        .map((character, index) => (
-          <SingleCharacter key={`postfix-${index}`} character={character} />
+    <div className={clsx("text-xl")}>
+      <AnimatePresence mode="popLayout">
+        {hourDigits.map((character, index) => (
+          <SingleCharacter
+            key={`h-${character}-${index}`}
+            character={character}
+          />
         ))}
+        <div key={"spacer1"} className="inline-block w-1" />
+        {minuteDigits.map((character, index) => (
+          <SingleCharacter
+            key={`m-${character}-${index}`}
+            character={character}
+          />
+        ))}
+        <div key={"spacer2"} className="inline-block w-1" />
+        {secondDigits.map((character, index) => (
+          <SingleCharacter
+            key={`s-${character}-${index}`}
+            character={character}
+          />
+        ))}
+        <div key={"spacer3"} className="inline-block w-1" />
+        {String(amOrPm)
+          .split("")
+          .map((character, index) => (
+            <SingleCharacter
+              key={`postfix-${character}-${index}`}
+              character={character}
+            />
+          ))}
+      </AnimatePresence>
     </div>
   );
 };
