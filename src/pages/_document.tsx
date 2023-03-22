5f0c7e63f062e0d2f470fc0c04acd9f2b0789db7
jackyef <jacky.efendi1@gmail.com>
2023-03-22 16:30:48 +0700

    Gradient bg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,17 @@
+import clsx from "clsx";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
+      <body
+        className={clsx(
+          "bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-50",
+          "bg-gradient-to-br from-slate-50 to-slate-200",
+          "dark:from-gray-900 dark:to-slate-900"
+        )}
+      >
         <Main />
         <NextScript />
       </body>
